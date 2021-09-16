373b736117d3d9c33f1ff89926a6a5c4c88ca76a
Narda16 <nardabrodovic@gmail.com>
2021-09-16 10:12:32 +0200

    Repo03 done

---
+++ ex02/src/index.js
@@ -0,0 +1,11 @@
+function main() {
+  // Only change code below this line
+  // This is a single line
+  /* This is a
+ multi-line code*/
+  // Only change code above this line
+}
+
+console.log(main());
+
+module.exports = main;
